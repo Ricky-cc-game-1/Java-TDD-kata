8f69eaf737df7a7a99c559150833294904a4bcde
rickyma <ricky.ma@convoypayments.com>
2018-11-26 13:07:22 +0800

    Done: 1,2,3
    In progress:
    4
    
    To Do:
    5,6,7,8,9

--- src/main/java/com/example/kata/StringCalculator/StringCalculator.java
+++ src/main/java/com/example/kata/StringCalculator/StringCalculator.java
@@ -2,6 +2,41 @@ package com.example.kata.StringCalculator;
 
 public class StringCalculator implements IStringCalculator {
 	public int Add(String numbers) {
-		return 0;
+		if(null == numbers)
+			return 0;
+		
+		String seperatorReg = "[,\n]";
+		String numberAfterProcess = numbers;
+		if(numbers.startsWith("//"))
+		{
+			String[] tmpArray = numbers.split("\n", 2)
+		}
+		
+		String[] numberList = numberAfterProcess.split(seperatorReg);
+					
+		if(numberList.length == 1)
+		{
+			return parseInt(numberList[0]);
+		}
+		else 
+		{
+			int r = 0;
+			for(String strTemp: numberList)
+			{
+				r += parseInt(strTemp);
+			}
+			return r;
+			
+		}
+		
+//		//return 0;
+	}
+	
+	protected int parseInt(String number)
+	{
+		if(null == number || "" == number)
+			return 0;
+		else
+			return Integer.parseInt(number);
 	}
 }
--- src/test/java/com/example/kata/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/example/kata/StringCalculator/StringCalculatorTest.java
@@ -8,40 +8,109 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class StringCalculatorTest {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(StringCalculatorTest.class);
-	
-	public IStringCalculator getCalculator()
-	{
+
+	public IStringCalculator getCalculator() {
 		return new StringCalculator();
 	}
 
 	@Test
-	public void testEmptyString()
-	{
+	public void testEmptyString() {
 		IStringCalculator calculator = this.getCalculator();
 		int result = calculator.Add("");
-		logger.info("result="+result);
-		Assert.assertEquals("test empty stirng", 0, result);
-	}
-	
-	@Test
-	public void testOneString()
-	{
-		IStringCalculator calculator = this.getCalculator();
-		int result = calculator.Add("2");
-		logger.info("result="+result);
-		Assert.assertEquals("test {2} stirng", 2, result);
+		logger.info("result=" + result);
+		Assert.assertEquals("test empty", 0, result);
 	}
 
 	@Test
-	public void testTwoString()
-	{
+	public void testOneString() {
+		IStringCalculator calculator = this.getCalculator();
+		
+		{
+			int result = calculator.Add("2");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {2}", 2, result);
+		}
+		
+		{
+			int result = calculator.Add("100");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {100}", 100, result);
+		}
+	}
+
+	@Test
+	public void testTwoString() {
 		IStringCalculator calculator = this.getCalculator();
 		int result = calculator.Add("1,2");
-		logger.info("result="+result);
-		Assert.assertEquals("test {1,2} stirng", 3, result);
+		logger.info("result=" + result);
+		Assert.assertEquals("test {1,2}", 3, result);
+
+	}
+
+	@Test
+	public void testManyString() {
+		IStringCalculator calculator = this.getCalculator();
 		
+		{
+			int result = calculator.Add("1,2,3,4,5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {1,2,3,4,5}", 15, result);
+		}
+		
+		{
+			int result = calculator.Add("14,5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {14,5}", 19, result);
+		}
+		
+		{
+			int result = calculator.Add("1,2,7,8,5,3,4,5,6,7,5,4,3");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {1,2,7,8,5,3,4,5,6,7,5,4,3}", 60, result);
+		}
 	}
 	
+	@Test
+	public void testNewLine() {
+		IStringCalculator calculator = this.getCalculator();
+		
+		{
+			int result = calculator.Add("1,2\n3\n4,5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {1,2\\n3\\n4,5}", 15, result);
+		}
+		
+		{
+			int result = calculator.Add("14\n5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {14\\n5}", 19, result);
+		}
+		
+	}	
+	
+	@Test
+	public void testSupportDelimitersChange() {
+		IStringCalculator calculator = this.getCalculator();
+		
+		{
+			int result = calculator.Add("//;\n1;2;3;4;5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {//;\\n1;2;3;4;5}", 15, result);
+		}
+		
+		{
+			int result = calculator.Add("//:\n14:5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {//:14:5}", 19, result);
+		}
+
+		{
+			int result = calculator.Add("//\n\n14\n5");
+			logger.info("result=" + result);
+			Assert.assertEquals("test {//:14:5}", 19, result);
+		}
+
+	}
 }
